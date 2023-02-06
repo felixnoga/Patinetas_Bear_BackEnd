baedb25234af6b5228e7a187d7fec029c7ce9180
PaolaGLupp <gldillana@gmail.com>
2023-02-06 20:13:31 +0100

    Register & Login Component

---
+++ src/controllers/registerController.js
@@ -0,0 +1,66 @@
+const startConnection = require("../config/connectiondb.js");
+
+const client = startConnection();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+
+    const { name, email, password } = req.body;
+    try {
+        const data = await client.query(`SELECT * FROM users WHERE email= $1;`, [email]);
+        const arr = data.rows;
+        if (arr.length != 0) {
+            return res.status(400).json({
+                error: "Email ya registrado", // HASTA AQUÍ BIEN 
+            });
+        } else {
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err)
+                    res.status(err).json({
+                        error: "Error en el servidor",
+                    });
+                const user = {
+                    name,
+                    email,
+                    password: hash,
+                };
+                var flag = 1;
+
+
+                client
+                    .query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3);`, 
+                    [user.name, user.email, user.password], (err) => {
+
+                        if (err) {
+                            flag = 0;
+                            console.error(err);
+                            return res.status(500).json({
+                                error: "Database error"
+                            })
+                        }
+                        else {
+                            flag = 1;
+                            res.status(200).send({ message: 'User añadido a la database' });
+                        }
+                    })
+                if (flag) {
+                    const token = jwt.sign(
+                        {
+                            email: user.email
+                        },
+                        process.env.SECRET_KEY
+                    );
+                };
+            });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: "Database error while registring user!",
+        });
+    };
+}
+
+module.exports = registerUser;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const cors = require("cors");
-const app = express()
+const validateToken = require('./middlewares/validateToken')
+require('dotenv').config();
+
+const app = express();
 const PORT = 3005
 
-app.use(express.json());
+//middlewares here
+app.use(express.json()); // this to get req.body
 app.use(cors());
+app.use('/secure-request', validateToken, /*routes*/);
+
+// ROUTES
+app.post('/login', require('./controllers/loginController'));
+app.post('/register', require("./controllers/registerController"))
+
 
 app.listen(PORT,
     () => { console.log(`server in port ${PORT}`) })
