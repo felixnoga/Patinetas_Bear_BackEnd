d95fcff93e7064ec20112f9b79aa5426bd8f2d33
PaolaGLupp <gldillana@gmail.com>
2023-02-10 18:32:25 +0100

    edition to user_name

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,8 +4,8 @@ const validateToken = (req, res, next) => {
     const token = req.header('Authorization')
     if (!token) return res.status(401).json({ error: 'Access denied' })
     try {
-        const userVerified = jwt.verify(token, process.env.SECRET)
-        req.user = userVerified
+        const userVerified = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = userVerified;
         next()
     } catch (error) {
         res.status(400).json({error: 'invalid token'})
