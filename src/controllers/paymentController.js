ec9cfdf21b7a297ff226aa756cfe81238ffc5b3e
Carlos isen <amra_akyra@hotmail.com>
2023-02-24 16:20:58 +0000

    feature: add strype model and controller

---
+++ src/controllers/paymentController.js
@@ -0,0 +1,17 @@
+const {paymentIntent} = require("../Models/strypeModels")
+
+const confirmPayment= async (req, res)=>{
+    let { id, amount } = req.body 
+    amount = parseFloat(amount)
+    try{
+        console.log(req.body, amount)
+        const payment= await paymentIntent({id, amount})
+        res.status(200).json({payment, message:"pago aceptado"})
+    }catch(error){
+        const message= error.raw ? error.raw.message : "ha habido un problema procesando su solicitud"
+        res.status(500).json({message})
+    }
+
+}
+
+module.exports= confirmPayment 
