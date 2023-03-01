f51d435b3d711b09de9e36c44ee7fc027549b583
Rocio Lanfranconi <rociolanfranconi@gmail.com>
2023-03-01 23:01:01 +0100

    actualizacion

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const tripControllers = require("../controllers/tripControler")
+const tripControllers = require("../controllers/tripController")
 
 router.post("/:id", tripControllers.completeTrip)
 router.get("/:id", tripControllers.getOnlyTrip)
