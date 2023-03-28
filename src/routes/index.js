3e5343cdb33e74cfdcdb87d836199e804bb81530
Felix <felixnoga@gmail.com>
2023-03-28 19:46:16 +0200

    To check AWS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const validateToken = require('../middlewares/validateToken')
+const validateToken = require('../middlewares/validateToken');
 const app = express();
+const secrets = JSON.parse(process.env.DB);
 
-
-app.use("/user", require('./user'));
+app.use('/user', require('./user'));
 // app.use("/client", require('./clients'));
 
+router.use('/scooters', validateToken, require('./scooters'));
+router.use('/booking', validateToken, require('./booking'));
+router.use('/trip', validateToken, require('./trips'));
+router.post(
+  '/payment/:id',
+  validateToken,
+  require('../controllers/paymentController')
+);
 
-router.use("/scooters",validateToken, require("./scooters"));
-router.use("/booking",validateToken, require("./booking"));
-router.use("/trip",validateToken, require("./trips"));
-router.post("/payment/:id",validateToken, require("../controllers/paymentController"))
-
-
-router.get("/", (req, res) => {
-    res.status(200).send("Thanks for login!");
+router.get('/', (req, res) => {
+  JSON.stringify(secrets);
+  res.status(200).send(secrets);
 });
-
-
 
 module.exports = router;
